666ddf1561a3fe497bfd87ab3959240a55917ff6
Justin Phelps <linuturk@onitato.com>
2014-08-04 11:30:39 -0500

    adding some necessary powershell scripts for Windows Configuration.

---
+++ webpagetest/powershell/Set-DisableUAC.ps1
@@ -0,0 +1,5 @@
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000
+    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+}
+Disable-UserAccessControl
