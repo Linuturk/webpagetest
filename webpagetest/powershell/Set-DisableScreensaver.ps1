0516e74c28409d1b1ea1369ed63c595da3777389
Justin Phelps <linuturk@onitato.com>
2014-08-04 13:46:57 -0500

    removing smart quotes

--- webpagetest/powershell/Set-DisableScreensaver.ps1
+++ webpagetest/powershell/Set-DisableScreensaver.ps1
@@ -1,3 +1,3 @@
-Set-ItemProperty -Path ‘HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\’ -Name ScreenSaveTimeOut -Value 0
-Set-ItemProperty -Path ‘HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\’ -Name ScreenSaveActive -Value 0
-Set-ItemProperty -Path ‘HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\’ -Name ScreenSaverIsSecure -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\' -Name ScreenSaveTimeOut -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\' -Name ScreenSaveActive -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Control Panel\Desktop\' -Name ScreenSaverIsSecure -Value 0
