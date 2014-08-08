00ee11519d799fc95068afb39060378d2fc44527
Justin Phelps <linuturk@onitato.com>
2014-08-08 14:53:49 -0500

    trying display name instead

--- webpagetest/powershell/Set-ClosePort445.ps1
+++ webpagetest/powershell/Set-ClosePort445.ps1
@@ -2,11 +2,11 @@ netsh advfirewall firewall delete rule name="WinRM" protocol=TCP localport=445
 
 netsh advfirewall firewall add rule name="WinRM" dir=in protocol=TCP localport=445 action=allow
 
-$CurrentVal = Get-NetFirewallRule -Name WinRM
+$CurrentVal = Get-NetFirewallRule -DisplayName WinRM
 Write-Output $CurrentVal
 
 if ($CurrentVal) {
-  Disable-NetFirewallRule -Name WinRM
+  Disable-NetFirewallRule -DisplayName WinRM
   Write-Output "changed=yes comment='Port 445 Disabled.'"
 } Else {
   Write-Output "changed=no comment='Port 445 Already Disabled.'"
