63cb0753bcf6bcee5ec395d86b47def2085dd222
Justin Phelps <linuturk@onitato.com>
2014-08-08 13:39:44 -0500

    putting in some checking to the Auto Logon

--- webpagetest/powershell/Set-AutoLogon.ps1
+++ webpagetest/powershell/Set-AutoLogon.ps1
@@ -1,3 +1,19 @@
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value "Administrator"
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value changeme
+$LogonPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+$Username = "{{ pillar['webpagetest']['win_user'] }}"
+$Password = "{{ pillar['webpagetest']['win_pass'] }}"
+
+$CurrentVal = Get-ItemProperty -Path $LogonPath -Name AutoAdminLogon
+
+If ($CurrentVal -eq 0) {
+  Set-ItemProperty -Path $LogonPath -Name AutoAdminLogon -Value 1
+  Set-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
+  Set-ItemProperty -Path $LogonPath -Name DefaultPassword -Value $Password
+} ElseIf ($CurrentVal -eq 1) {
+  Set-ItemProperty -Path $LogonPath -Name AutoAdminLogon -Value 1
+  Set-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
+  Set-ItemProperty -Path $LogonPath -Name DefaultPassword -Value $Password
+} Else {
+  New-ItemProperty -Path $LogonPath -Name AutoAdminLogon -Value 1
+  New-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
+  New-ItemProperty -Path $LogonPath -Name DefaultPassword -Value $Password
+}
