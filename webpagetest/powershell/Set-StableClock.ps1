0c2a9148cf97b0583d3a47fb84cb158969c88bd5
Justin Phelps <linuturk@onitato.com>
2014-08-08 15:38:51 -0500

    adding changed=yes

--- webpagetest/powershell/Set-StableClock.ps1
+++ webpagetest/powershell/Set-StableClock.ps1
@@ -2,7 +2,8 @@ $allthings = bcdedit | Select-String -pattern "useplatformclock        Yes"
 Write-Output $allthings
 
 if ($useplatformclock) {
-  Write-Output "changed=no comment='Platform Clock Already Enabled'"
+  Write-Output "changed=no comment='Platform Clock Already Enabled.'"
 } Else {
   Start-Process -wait bcdedit -ArgumentList "/set {default}, useplatformclock true"
+  Write-Output "changed=yes comment='Platform Clock Enabled.'"
 }
