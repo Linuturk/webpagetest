b5873d649541139343d4b8735b148f62c8ff7236
Justin Phelps <linuturk@onitato.com>
2014-08-08 13:54:21 -0500

    adding output

--- webpagetest/powershell/Set-DisableScreensaver.ps1
+++ webpagetest/powershell/Set-DisableScreensaver.ps1
@@ -4,4 +4,7 @@ $CurrentVal = Get-ItemProperty -Path $Path -Name ScreenSaveActive
 
 if ($CurrentVal -ne 0) {
   Set-ItemProperty -Path $Path -Name ScreenSaveActive -Value 0
+  Write-Output "Screensaver Disabled."
+} Else {
+  Write-Output "Screensaver Already Disabled."
 }
