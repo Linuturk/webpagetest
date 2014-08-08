677f8003647f5b5c3dd0c2993e035096496fc9f2
Justin Phelps <linuturk@onitato.com>
2014-08-08 15:07:52 -0500

    testing the if statement

--- webpagetest/powershell/Set-ClosePort445.ps1
+++ webpagetest/powershell/Set-ClosePort445.ps1
@@ -2,7 +2,7 @@ $CurrentVal = Get-NetFirewallRule -DisplayName WinRM
 Write-Output $CurrentVal
 
 if ($CurrentVal.Enabled) {
-  Disable-NetFirewallRule -DisplayName WinRM
+  Remove-NetFirewallRule -DisplayName WinRM
   Write-Output "changed=yes comment='Port 445 Disabled.'"
 } Else {
   Write-Output "changed=no comment='Port 445 Already Disabled.'"
