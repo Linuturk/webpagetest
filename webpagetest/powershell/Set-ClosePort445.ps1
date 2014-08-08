97c13979e60c9cd8a5e1071ed66e97a055c422ea
Justin Phelps <linuturk@onitato.com>
2014-08-08 14:47:55 -0500

    Trying to make the firewall stateful

--- webpagetest/powershell/Set-ClosePort445.ps1
+++ webpagetest/powershell/Set-ClosePort445.ps1
@@ -1,1 +1,12 @@
 netsh advfirewall firewall delete rule name="WinRM" protocol=TCP localport=445
+
+netsh advfirewall firewall add rule name="WinRM" dir=in protocol=TCP localport=445 action=allow
+
+$CurrentVal = Get-NetFirewallRule -Name WinRM
+Write-Output $CurrentVal
+
+if ($CurrentVal) {
+  Disable-NetFirewallRule -Name WinRM
+  Write-Output "changed=yes comment='Port 445 Disabled.'"
+} Else {
+  Write-Output "changed=no comment='Port 445 Already Disabled.'"
