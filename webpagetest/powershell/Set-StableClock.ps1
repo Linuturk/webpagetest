1ad5a61ded3f0d81b1108b9a3fa3906544abf374
Justin Phelps <linuturk@onitato.com>
2014-08-08 15:41:32 -0500

    calling bcdedit directly instead of start-process

--- webpagetest/powershell/Set-StableClock.ps1
+++ webpagetest/powershell/Set-StableClock.ps1
@@ -4,6 +4,6 @@ Write-Output $allthings
 if ($useplatformclock) {
   Write-Output "changed=no comment='Platform Clock Already Enabled.'"
 } Else {
-  Start-Process -wait bcdedit -ArgumentList "/set {default}, useplatformclock true"
+  bcdedit /set  useplatformclock true
   Write-Output "changed=yes comment='Platform Clock Enabled.'"
 }
