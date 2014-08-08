04f186c697f12ba32edcda3e5c73f571033f07b0
Justin Phelps <linuturk@onitato.com>
2014-08-08 15:35:56 -0500

    first attempt at filtering output

--- webpagetest/powershell/Set-StableClock.ps1
+++ webpagetest/powershell/Set-StableClock.ps1
@@ -1,3 +1,3 @@
 Start-Process -wait bcdedit -ArgumentList "/set {default}, useplatformclock true"
-$allthings = bcdedit
+$allthings = bcdedit | Select-String -pattern "useplatformclock        Yes"
 Write-Output $allthings
