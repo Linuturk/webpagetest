86d6e0c45115dd2728e63dc81b3bc5b96eeb7ac8
Justin Phelps <linuturk@onitato.com>
2014-08-11 13:31:35 -0500

    adding necessary quoting

--- webpagetest/powershell/Set-InstallAviSynth.ps1
+++ webpagetest/powershell/Set-InstallAviSynth.ps1
@@ -5,6 +5,6 @@ $Installed = Test-Path "C:\Program Files (x86)\AviSynth 2.5" -pathType container
 If ($Installed) {
   Write-Output "changed=no comment='AviSynth already installed.'"
 } Else {
-  $InstallDir\agent\Avisynth_258.exe /S
+  & "$InstallDir\agent\Avisynth_258.exe" /S
   Write-Output "changed=yes comment='AviSynth installed.'"
 }
