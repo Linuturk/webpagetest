7aae5958dfbf114ac2884fb625704f198ccb6354
Justin Phelps <linuturk@onitato.com>
2014-08-11 14:23:05 -0500

    adding same try catch finally to screensaver

--- webpagetest/powershell/Set-DisableScreensaver.ps1
+++ webpagetest/powershell/Set-DisableScreensaver.ps1
@@ -1,11 +1,15 @@
 $Path = 'HKCU:\Control Panel\Desktop'
 
-$CurrentVal = Get-ItemProperty -Path $Path -Name ScreenSaveActive
-Write-Output $CurrentVal
-
-if ($CurrentVal.ScreenSaveActive -ne 0) {
-  Set-ItemProperty -Path $Path -Name ScreenSaveActive -Value 0
-  Write-Output "changed=yes comment='Screensaver Disabled.'"
-} Else {
-  Write-Output "changed=no comment='Screensaver Already Disabled.'"
+Try {
+  $CurrentVal = Get-ItemProperty -Path $Path -Name ScreenSaveActive
+  Write-Output $CurrentVal
+} Catch {
+  $CurrentVal = False
+} Finally {
+  if ($CurrentVal.ScreenSaveActive -ne 0) {
+    Set-ItemProperty -Path $Path -Name ScreenSaveActive -Value 0
+    Write-Output "changed=yes comment='Screensaver Disabled.'"
+  } Else {
+    Write-Output "changed=no comment='Screensaver Already Disabled.'"
+  }
 }
