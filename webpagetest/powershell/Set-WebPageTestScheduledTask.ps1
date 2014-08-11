97195edf47f0d917ad9b3695d9e7d47d4e0b69b6
Justin Phelps <linuturk@onitato.com>
2014-08-11 11:59:55 -0500

    adding a scheduled task for the wptdriver

---
+++ webpagetest/powershell/Set-WebPageTestScheduledTask.ps1
@@ -0,0 +1,18 @@
+$Host = {{ grains.host }}
+$User = {{ pillar['webpagetest']['win_user'] }}
+$InstallDir = "{{ pillar['webpagetest']['win_install_dir']\agent"
+
+$GetTask = Get-ScheduledTask -TaskName "wptdriver"
+Write-Output $GetTask
+
+if ($GetTask) {
+  Write-Output "changed=no comment='Task already scheduled.'"
+} Else {
+  $A = New-ScheduledTaskAction –Execute "$InstallDir\wptdriver.exe"
+  $T = New-ScheduledTaskTrigger -AtLogon
+  $P = "$Host\$User"
+  $S = New-ScheduledTaskSettingsSet
+  $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+  Register-ScheduledTask -TaskName "wptdriver" -InputObject $D
+  Write-Output "changed=yes comment='Task scheduled.'"
+}
