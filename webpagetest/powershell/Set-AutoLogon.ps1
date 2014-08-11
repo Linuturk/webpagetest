042c8c11c47f98ade93eb9b1fbc809d1db694c20
Justin Phelps <linuturk@onitato.com>
2014-08-11 14:30:32 -0500

    adding additional logic for the credential changes

--- webpagetest/powershell/Set-AutoLogon.ps1
+++ webpagetest/powershell/Set-AutoLogon.ps1
@@ -4,11 +4,17 @@ $Password = "{{ pillar['webpagetest']['win_pass'] }}"
 
 $CurrentVal = Get-ItemProperty -Path $LogonPath -Name AutoAdminLogon
 
-If ($CurrentVal.AutoAdminLogon -ne 0) {
-  Set-ItemProperty -Path $LogonPath -Name AutoAdminLogon -Value 1
-  Set-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
-  Set-ItemProperty -Path $LogonPath -Name DefaultPassword -Value $Password
-  Write-Output "changed=no comment='AutoLogon already enabled.'"
+If ($CurrentVal.AutoAdminLogon -eq 1) {
+  $CurrentUser = Get-ItemProperty -Path $LogonPath -Name DefaultUserName
+  $CurrentPass = Get-ItemProperty -Path $LogonPath -Name DefaultPassword
+
+  If ($CurrentUser -ne $Username -Or $CurrentPass -ne $Password) {
+    Set-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
+    Set-ItemProperty -Path $LogonPath -Name DefaultPassword -Value $Password
+    Write-Output "changed=yes comment='Credentials Updated.'"
+  } Else {
+    Write-Output "changed=no comment='AutoLogon already enabled.'"
+  }
 } Else {
   Set-ItemProperty -Path $LogonPath -Name AutoAdminLogon -Value 1
   New-ItemProperty -Path $LogonPath -Name DefaultUserName -Value $Username
