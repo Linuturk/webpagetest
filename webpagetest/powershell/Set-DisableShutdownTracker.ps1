16179185d2260a1520271677083b7c19146927c8
Justin Phelps <linuturk@onitato.com>
2014-08-11 14:21:43 -0500

    try catch for the shutdown tracker

--- webpagetest/powershell/Set-DisableShutdownTracker.ps1
+++ webpagetest/powershell/Set-DisableShutdownTracker.ps1
@@ -1,11 +1,15 @@
 $Path = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Reliability'
 
-$CurrentVal = Get-ItemProperty -Path $Path -Name ShutdownReasonUI
-Write-Output $CurrentVal
-
-if ($CurrentVal.ShutdownReasonUI -ne 0) {
-  Set-ItemProperty -Path $Path -Name ShutdownReasonUI -Value 0
-  Write-Output "changed=yes comment='Shutdown Tracker Disabled.'"
-} Else {
-  Write-Output "changed=no comment='Shutdown Tracker Already Disabled.'"
+Try {
+  $CurrentVal = Get-ItemProperty -Path $Path -Name ShutdownReasonUI
+  Write-Output $CurrentVal
+} Catch {
+  $CurrentVal = False
+} Finally {
+  if ($CurrentVal.ShutdownReasonUI -ne 0) {
+    New-ItemProperty -Path $Path -Name ShutdownReasonUI -Value 0
+    Write-Output "changed=yes comment='Shutdown Tracker Disabled.'"
+  } Else {
+    Write-Output "changed=no comment='Shutdown Tracker Already Disabled.'"
+  }
 }
