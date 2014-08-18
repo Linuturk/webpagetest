6a5904c6b716ef1fdfda978f15b4b967918323da
Justin Phelps <linuturk@onitato.com>
2014-08-18 14:02:39 -0500

    removing the download file function, as salt is placing the file now

--- webpagetest/powershell/Set-InstallDummyNet.ps1
+++ webpagetest/powershell/Set-InstallDummyNet.ps1
@@ -7,16 +7,6 @@ if ($testsigning) {
   Write-Output "changed=yes comment='Test Signing Enabled.'"
 }
 
-Function download_file ($url, $localpath, $filename){
-    if(!(Test-Path -Path $localpath)){
-        New-Item $localpath -type directory > $null
-    }
-    $webclient = New-Object System.Net.WebClient;
-    $webclient.DownloadFile($url, $localpath + "\" + $filename)
-}
-
-$mindinst_file = "http://9cecab0681d23f5b71fb-642758a7a3ed7927f3ce8478e9844e11.r45.cf5.rackcdn.com/mindinst.exe"
-download_file $mindinst_file "C:\webpagetest" "mindinst.exe"
 cd C:\webpagetest
 
 Write-Output "Install ipfw+dummynet drivers on the all interface"
