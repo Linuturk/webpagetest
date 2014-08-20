975923359cff14ecc22041b22c6e4c99e63ede3a
Justin Phelps <linuturk@onitato.com>
2014-08-20 14:53:09 -0500

    adding powershell script for Server Manager disabling #11

---
+++ webpagetest/powershell/Set-DisableServerManager.ps1
@@ -0,0 +1,8 @@
+$CurrentState = Get-ScheduledTask -TaskName "ServerManager" | Select-String -pattern "Ready"
+
+If ($CurrentState) {
+  Get-ScheduledTask -TaskName "ServerManager" | Disable-ScheduledTask
+  Write-Output "changed=yes comment='Server Manager disabled at logon.'"
+} Else {
+  Write-Output "changed=no comment='Server Manager already disabled at logon.'"
+}
