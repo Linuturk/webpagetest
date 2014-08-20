b01fb59c7a07675365fd157a84348f86bacfccef
Justin Phelps <linuturk@onitato.com>
2014-08-20 14:12:15 -0500

    adding in the spacing on the if check

--- webpagetest/powershell/Set-MonitorTimeout.ps1
+++ webpagetest/powershell/Set-MonitorTimeout.ps1
@@ -1,6 +1,6 @@
 $CurrentVal = POWERCFG /QUERY SCHEME_BALANCED SUB_VIDEO VIDEOIDLE | Select-String -pattern "Current AC Power Setting Index:"
 
-If ($CurrentVal -eq "Current AC Power Setting Index: 0x00000000") {
+If ($CurrentVal -eq "    Current AC Power Setting Index: 0x00000000") {
   Write-Output "changed=no comment='Display Timeout already set to Never.'"
 } Else {
   POWERCFG /CHANGE -monitor-timeout-ac 0
