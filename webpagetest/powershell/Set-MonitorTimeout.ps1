a3451e458c07a1cad14f74b4f90ce565931a83ff
Justin Phelps <linuturk@onitato.com>
2014-08-20 14:21:31 -0500

    adding some debugging and switching to -like

--- webpagetest/powershell/Set-MonitorTimeout.ps1
+++ webpagetest/powershell/Set-MonitorTimeout.ps1
@@ -1,6 +1,7 @@
 $CurrentVal = POWERCFG /QUERY SCHEME_BALANCED SUB_VIDEO VIDEOIDLE | Select-String -pattern "Current AC Power Setting Index:"
+Write-Output $CurrentVal
 
-If ($CurrentVal -eq "    Current AC Power Setting Index: 0x00000000") {
+If ($CurrentVal -like "0x00000000") {
   Write-Output "changed=no comment='Display Timeout already set to Never.'"
 } Else {
   POWERCFG /CHANGE -monitor-timeout-ac 0
