99b777be8976b52ab7985d5c38c2e1f84e135987
Justin Phelps <linuturk@onitato.com>
2014-08-21 15:49:16 -0500

    arg smartquotes #8

--- webpagetest/powershell/Set-AutoLogon.ps1
+++ webpagetest/powershell/Set-AutoLogon.ps1
@@ -37,7 +37,7 @@ function AddAuditToRegKey {
 
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_before.txt' -Width 200 -Append
     $RegKey_ACL = Get-Acl $key
-    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","SetValue,CreateSubKey,Delete”,"none","none","Success")
+    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","SetValue,CreateSubKey,Delete","none","none","Success")
     $RegKey_ACL.AddAuditRule($AccessRule)
     $RegKey_ACL | Set-Acl $key
     Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_after.txt' -Width 200 -Append
