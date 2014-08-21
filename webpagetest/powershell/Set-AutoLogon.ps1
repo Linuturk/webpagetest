a5663f524352ffe83b316735b46d36253c3545e8
Justin Phelps <linuturk@onitato.com>
2014-08-21 15:37:49 -0500

    adding auditing to the key #8

--- webpagetest/powershell/Set-AutoLogon.ps1
+++ webpagetest/powershell/Set-AutoLogon.ps1
@@ -27,3 +27,20 @@ If ($CurrentVal.AutoAdminLogon -eq "1") {
   Set-ItemProperty -Path $LogonPath -Name LastUsedUsername -Value "$Username"
   Write-Output "changed=yes comment='AutoLogon enabled.'"
 }
+
+function AddAuditToRegKey {
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$key
+    )
+
+    Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_before.txt' -Width 200 -Append
+    $RegKey_ACL = Get-Acl $key
+    $AccessRule = New-Object System.Security.AccessControl.RegistryAuditRule("Everyone","SetValue,CreateSubKey,Delete”,"none","none",”Success")
+    $RegKey_ACL.AddAuditRule($AccessRule)
+    $RegKey_ACL | Set-Acl $key
+    Get-Acl $key -Audit | Format-List Path,AuditToString | Out-File -FilePath 'reg_after.txt' -Width 200 -Append
+}
+
+AddAuditToRegKey $LogonPath
