f963a7ecf5b2c8def768fa6e4b3a1f2b7a465fdf
Justin Phelps <linuturk@onitato.com>
2014-08-25 10:46:51 -0500

    overwrite all files #16

--- heat/Agent-Updater.ps1
+++ heat/Agent-Updater.ps1
@@ -1,7 +1,7 @@
 Function Set-Agent-Updater ($UpdateURL, $Destination, $FileName) {
   Invoke-WebRequest $UpdateURL -OutFile "$Destination\$FileName.zip"
   $Shell = New-Object -com shell.application
-  $Shell.NameSpace($Destination).copyhere("$Destination\$FileName.zip\$FileName.ini")
+  $Shell.NameSpace($Destination).copyhere("$Destination\$FileName.zip\$FileName.ini", 16)
 }
 
 Set-Agent-Updater -UpdateURL "http://www.webpagetest.org/work/update/update.zip" -Destination "C:\wpt-www\work\update" -FileName "update"
