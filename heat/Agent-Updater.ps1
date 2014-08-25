494094117559e751b92f65bdd15d1ac7b26817cc
Justin Phelps <linuturk@onitato.com>
2014-08-25 10:11:34 -0500

    refactoring based on some testing #16

--- heat/Agent-Updater.ps1
+++ heat/Agent-Updater.ps1
@@ -1,10 +1,8 @@
-Function Set-Update-Agents ($UpdateURL, $INIFile, $ZipFile, $Destination) {
+Function Set-Agent-Updater ($UpdateURL, $INIFile, $Destination) {
   Invoke-WebRequest $UpdateURL -OutFile $Destination
-
   $Shell = New-Object -com shell.application
-  $Zip = $Shell.NameSpace("$Destination\$ZipFile")
   $Shell.NameSpace($Destination).copyhere($INIFile)
 }
 
-Set-Update-Agents -UpdateURL "http://www.webpagetest.org/work/update/update.zip" -INIFile "update.ini" -ZipFile "update.zip" -Destination "C:\wpt-www\work\update\"
-Set-Update-Agents -UpdateURL "http://www.webpagetest.org/work/update/wptupdate.zip" -INIFile "wptupdate.ini" -ZipFile "wptupdate.zip" -Destination "C:\wpt-www\work\update\"
+Set-Agent-Updater -UpdateURL "http://www.webpagetest.org/work/update/update.zip" -INIFile "update.ini" -Destination "C:\wpt-www\work\update\update.zip"
+Set-Agent-Updater -UpdateURL "http://www.webpagetest.org/work/update/wptupdate.zip" -INIFile "wptupdate.ini" -Destination "C:\wpt-www\work\update\wptdriver.zip"
