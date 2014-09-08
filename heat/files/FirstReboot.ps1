5b774e1f38d0b35071754042ce73eda69f0c6085
Justin Phelps <linuturk@onitato.com>
2014-09-08 15:30:11 -0500

    trimming some fat. Start-Process reboot doesn't work apparently

--- heat/files/FirstReboot.ps1
+++ heat/files/FirstReboot.ps1
@@ -1,21 +1,14 @@
 Start-Sleep -s 60
 
-# Reboot the first time the computer starts after the HEAT automation
-#If (Test-Path C:\wpt-temp\firstreboot -pathType leaf) {
-#  New-Item C:\wpt-temp\firstreboot -type file
-#  Restart-Computer
-#} Else {
-  # Install DummyNET after this first reboot so testsigning is enabled
-  $InstallDir = "C:\wpt-agent"
-  $dummynet = Get-NetAdapterBinding -Name public*
+$InstallDir = "C:\wpt-agent"
+$dummynet = Get-NetAdapterBinding -Name public*
 
-  If ($dummynet.ComponentID -eq "ipfw+dummynet") {
-    Write-Output "Already Enabled."
-  } Else {
-    Import-Certificate -FilePath C:\wpt-agent\WPOFoundation.cer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
-    cd $InstallDir
-    .\mindinst.exe C:\wpt-agent\dummynet\64bit\netipfw.inf -i -s 
-    Enable-NetAdapterBinding -Name public0 -DisplayName ipfw+dummynet 
-    Enable-NetAdapterBinding -Name private0 -DisplayName ipfw+dummynet
-  }
-#}
+If ($dummynet.ComponentID -eq "ipfw+dummynet") {
+  Write-Output "Already Enabled."
+} Else {
+  Import-Certificate -FilePath C:\wpt-agent\WPOFoundation.cer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+  cd $InstallDir
+  .\mindinst.exe C:\wpt-agent\dummynet\64bit\netipfw.inf -i -s 
+  Enable-NetAdapterBinding -Name public0 -DisplayName ipfw+dummynet 
+  Enable-NetAdapterBinding -Name private0 -DisplayName ipfw+dummynet
+}
